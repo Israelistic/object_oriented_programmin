32295f83c5a10ade5bfb87cde9227468bfc192f4
hlerman <haguyle@gmail.com>
2018-05-31 09:43:17 -0400

    ==CHANGE LOG=== ver 1.0 an instance method initialize an instance method borrow an instance method return_to_library an instance method lent_out? a class method create a class method current_due_date a class method overdue_books a class method browse a class method available a class method borrowed

--- Book_Lending.rb
+++ Book_Lending.rb
@@ -1,9 +1,45 @@
+#!/usr/bin/env ruby
+
 class Book
 
 @@on_shelf =[]
 @on_loan =[]
 
+    def initialize
+        @borrow
+        @return_to_library
+        @lent_out
+        @books_title
+        @author
+        @isbn
+    end
+    def self.create()
 
+    end
+    def self.current_due_date
+
+    end
+    def self.overdue_books
+
+    end
+    def self.browse
+
+    end
+
+    def self.available
+
+    end
+
+    def self.borrowed
+
+    end
+
+    def due_date  #reader
+        return due_date
+    end
+    def due_date_r(due_date) #writer
+        due_date
+    end
 
 
 
