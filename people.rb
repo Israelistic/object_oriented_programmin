53b64ad8ba0c02e1ac1113b57a7b7ae62c71cfde
hlerman <haguyle@gmail.com>
2018-06-05 17:42:36 -0400

    added a mathod to teacher and student

--- people.rb
+++ people.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/env ruby
+class Student
+
+    def learn
+        return "I get it"
+    end
+
+end
+
+
+class instructor
+    def teach
+        return "Everythin in Ruby is an Object"
+    end
+
+end
