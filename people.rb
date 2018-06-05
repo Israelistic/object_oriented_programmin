b1cc5c0626e12608f82aa9422892c0aa86b735c4
hlerman <haguyle@gmail.com>
2018-06-05 18:42:10 -0400

    added Inheritance to classes Student and Instractur

--- people.rb
+++ people.rb
@@ -1,26 +1,41 @@
 #!/usr/bin/env ruby
-class Student
 
-    def learn
-        return "I get it"
-    end
-
-end
-
-
-class instructor
-    def teach
-        return "Everythin in Ruby is an Object"
-    end
-
-end
 
 class Person
 
     def initialize(name)
         @name = name
     end
-    def greeting(name)
-        p "Hi, my name is #{name}".
+
+    def greeting
+          p "Hi, my name is #{@name}"
     end
 end
+
+class Student < Person
+
+    def learn
+        return "I get it"
+    end
+end
+
+
+
+class Instructor < Person
+
+    def teach
+        return "Everythin in Ruby is an Object"
+    end
+
+end
+
+
+ p nadia = Instructor.new("nadia")
+
+  nadia.greeting
+p chris = Student.new("Chris")
+
+p
+
+# p chris.teach # the student class is not inheritance.
+#the parent class have to be placed at the top of the filke in order for ruby to work 
