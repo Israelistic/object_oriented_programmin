f4163675c8c4a8972ae4876f8a15b75a14a47c86
hlerman <haguyle@gmail.com>
2018-06-07 00:13:05 -0400

    added the readers and writers

--- jun4_monday_oop.rb
+++ jun4_monday_oop.rb
@@ -1,18 +1,14 @@
 #!/usr/bin/env ruby
 
-class Task
 
-    def initialize
-        @description =""
-        @due_date =""
+#Exercise 1
+class Task
+attr_accessor :due_date, :description
+    def initialize(desc, due_date)
+        @description = desc
+        @due_date = due_date
     end
-    # reader
-    def description
-        return @description
-    end
-    def due_date
-        return @due_date
-    end
+
 
     #Writer
     def description=(desc)
@@ -22,3 +18,28 @@ class Task
         @due_date = due_date
     end
 end
+#Exercise 2
+class TodoList < Task
+
+
+    def initialize
+        @add_task = []
+    end
+    def add_tasks(add_task)
+        @add_task << add_task
+    end
+    def read_tasks
+        return @@add_task
+    end
+end
+
+
+
+
+p task1 = Task.new("Learn how to work with OOP", 10-06-2018)
+p task2 = Task.new("Start SQL code", 07-06-2018)
+p task3 = Task.new("keep pushing", 07-06-2018)
+p todo = TodoList.new
+p todo.add_tasks(task1)
+p todo.add_tasks(task2)
+p todo.add_tasks(task3)
